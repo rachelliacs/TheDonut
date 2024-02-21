e37b1ee1e77c1a15e8c3f658d829958d7471e1b9
rachelliacs <rachellia.citra13@gmail.com>
2024-02-22 03:06:20 +0700

    front end: update checkout

---
+++ application/controllers/CheckoutConfirmation.php
@@ -0,0 +1,36 @@
+<?php
+class CheckoutConfirmation extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Auth');
+        $this->load->model('Data');
+        $this->load->library('session');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Checkout Confirmation';
+        $table = 'tb_store';
+
+        $this->load->database();
+
+        $data['storedatas'] = $this->Data->getStoreData($table);
+        $StoreName = '';
+        if (!empty($data['storedatas'])) {
+            $StoreName = $data['storedatas'][0]['storeName'];
+        }
+        $data['StoreName'] = $StoreName;
+
+        // Check if user is logged in
+        if (!$this->session->userdata('logged_in')) {
+            // User is not logged in, redirect to login page
+            redirect('login');
+        } else {
+            $this->load->view('user/templates/header', $data);
+            $this->load->view('user/pages/checkoutConfirmation', $data);
+            $this->load->view('user/templates/footer');
+        }
+    }
+}
---
+++ application/views/user/pages/checkoutConfirmation.php
@@ -0,0 +1,7 @@
+<section class="section padding-large">
+    <div class="container">
+        <h1>Checkout Confirmation
+
+        </h1>
+    </div>
+</section>
